865f16020d43fbb615805bf101939e0dd7a2e9e6
Alexander Truong <alexander.m.truong@gmail.com>
2018-07-11 17:46:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,13 +7,3 @@ function theBeatlesPlay(musicians, instruments) {
     
     return arr
 }
-
-
-//function theBeatlesPlay(musicians, instruments) {
-  const barr = [];
-  
-  for (let i = 0, l = musicans.length; i < l; i++) { 
-    barr.push('${musicians[i]} plays ${instruments[i]}');
-}
-return barr;
-}
