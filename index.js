ee9081d45b793b0cfd2ee3e7013b4bb310608d11
Alexander Truong <alexander.m.truong@gmail.com>
2018-07-11 17:04:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,26 +1,4 @@
 // add solution here
-//const facts = [
-// "He was the last Beatle to learn to drive",
-////  "He was never a vegetarian",
-//"He was a choir boy and a boy scout",
-//"He hated the sound of his own voice"
-// ]
-
-//var musicians = [
-///  "John Lennon",
-//  "Paul McCartney",
-//  "George Harrison",
-//  "Ringo Starr"
-//  ]
-
-//var instruments = [
- // "Guitar",
- // "Bass Guitar",
- // "Lead Guitar",
- // "Drums"
- // ]
-
-
 function theBeatlesPlay(musicians, instruments) {
 var array = [];
 for (let i = 0; i < musicians.length; i++) { 
