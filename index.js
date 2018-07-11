682f5674a2a531d6acdec3a667ec53bd8661ac98
Alexander Truong <alexander.m.truong@gmail.com>
2018-07-11 17:37:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
 function theBeatlesPlay(musicians, instruments) {
-const array = []
-for (let i = 0; i < musicans.length; i++) { 
-   array.push('${musicians[i]} plays ${instruments[i]}')
+  const array = []
+  
+  for (let i = 0, 1 = musicans.length; i < 1; i++) { 
+    array.push('${musicians[i]} plays ${instruments[i]}')
 }
 return array
 }
