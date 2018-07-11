7efafd2baff37844bc2a87043c8a2e0592c9e77b
Alexander Truong <alexander.m.truong@gmail.com>
2018-07-11 16:40:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // add solution here
+var musicians = [
+  "John Lennon",
+  "Paul McCartney",
+  "George Harrison",
+  "Ringo Starr"
+  ]
+
+var instruments - [
+  "Guitar",
+  "Guitar",
+  "Drums",
+  "Bass"
+  ]
