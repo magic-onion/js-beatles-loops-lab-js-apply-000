98c2754642277bb7d7a6b2eef15c6fb5f1f22c73
Alexander Truong <alexander.m.truong@gmail.com>
2018-07-11 16:53:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // add solution here
 const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and a boy scout",
-  "He hated the sound of his own voice"
-  ]
+//  "He was the last Beatle to learn to drive",
+//  "He was never a vegetarian",
+// // "He was a choir boy and a boy scout",
+ // "He hated the sound of his own voice"
+ // ]
 
-var musicians = [
-//  "John Lennon",
+//var musicians = [
+///  "John Lennon",
 //  "Paul McCartney",
 //  "George Harrison",
 //  "Ringo Starr"
