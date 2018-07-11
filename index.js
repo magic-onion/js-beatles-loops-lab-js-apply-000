c7e6df604df78814066aeca54ba0e83b3160e176
Alexander Truong <alexander.m.truong@gmail.com>
2018-07-11 17:06:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // add solution here
+var musicians = [
+  "paul",
+  "john"
+  ]
+
 function theBeatlesPlay(musicians, instruments) {
 var array = [];
 for (let i = 0; i < musicians.length; i++) { 
